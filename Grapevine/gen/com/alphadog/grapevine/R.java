0e6b216d438004b3ed907f73f23b196d858a1591
Priyank Gupta <priyaaank@gmail.com>
2010-09-15 16:22:15 +0530

    Added some view xmls which may not be relevent immediatly. And also some minor changes.

--- Grapevine/gen/com/alphadog/grapevine/R.java
+++ Grapevine/gen/com/alphadog/grapevine/R.java
@@ -13,11 +13,17 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int review_box=0x7f050000;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int dashboard=0x7f030000;
+        public static final int review=0x7f030001;
+        public static final int review_list=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int create_sqls=0x7f040001;
+        public static final int create_sqls=0x7f040002;
+        public static final int no_reviews=0x7f040001;
     }
 }
--- Grapevine/src/com/alphadog/grapevine/Dashboard.java
+++ Grapevine/src/com/alphadog/grapevine/Dashboard.java
@@ -8,6 +8,6 @@ public class Dashboard extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.dashboard);
     }
 }

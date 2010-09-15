95b5b4fdf9c1e7680dd6f30cbed7fe44afc5d15b
Shishir Das <shishir.das@gmail.com>
2010-09-15 17:53:47 +0530

    Shishir:- trying out.. random stuff.. will revert most of it :)

--- Grapevine/gen/com/alphadog/grapevine/R.java
+++ Grapevine/gen/com/alphadog/grapevine/R.java
@@ -11,10 +11,19 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int android_button=0x7f020000;
+        public static final int android_focused=0x7f020001;
+        public static final int android_normal=0x7f020002;
+        public static final int android_pressed=0x7f020003;
+        public static final int icon=0x7f020004;
+    }
+    public static final class id {
+        public static final int button=0x7f050000;
+        public static final int edittext=0x7f050001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int ooga=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
--- Grapevine/src/com/alphadog/grapevine/Dashboard.java
+++ Grapevine/src/com/alphadog/grapevine/Dashboard.java
@@ -2,6 +2,10 @@ package com.alphadog.grapevine;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Dashboard extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +13,14 @@ public class Dashboard extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				Log.v("cliekced", "clicked");
+				setContentView(R.layout.ooga);
+			}
+		});
     }
 }

ee336004b5c4b468a94c5366042227f7afb81a08
Vinay Patel <mail@vinayvinay.com>
2010-09-18 16:30:54 +0530

    fixing fade data script

--- grapevine_ws/db/fake_data.rb
+++ grapevine_ws/db/fake_data.rb
@@ -3,7 +3,7 @@ require 'sequel'
 
 reviews = Sequel.connect('sqlite://grapevine.db').from(:reviews)
 
-(ARGV.first.to_i || 45).times do
+[ARGV.first.to_i, 15].max.times do
 	reviews.insert(:image_url => "http://www.#{Faker::Internet.domain_name}/#{rand(99999)}",
 								 :text => Faker::Lorem.paragraph,
 								 :latitude => rand(999999) / 1000.0,

9e043e560b11c54e20022d020f128176d2d2f2c9
Vinay Patel <mail@vinayvinay.com>
2010-09-18 18:44:51 +0530

    limiting fake texts to 200 chars to support postgres sync.

--- grapevine_ws/db/fake_data.rb
+++ grapevine_ws/db/fake_data.rb
@@ -8,7 +8,7 @@ reviews = db.from(:reviews)
 
 [ARGV.first.to_i, 15].max.times do
 	reviews.insert(:image_url => "http://www.#{Faker::Internet.domain_name}/#{rand(99999)}",
-								 :text => Faker::Lorem.paragraph,
+								 :text => Faker::Lorem.paragraph[0..200],
 								 :latitude => rand(999999) / 1000.0,
 								 :longitude => rand(999999) / 1000.0)
 end

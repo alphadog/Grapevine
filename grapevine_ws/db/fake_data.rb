0b2092f2def823374a514cafba55931a24c1538a
Vinay Patel <mail@vinayvinay.com>
2010-09-20 19:44:19 +0530

    dispersing points in reviews.

--- grapevine_ws/db/fake_data.rb
+++ grapevine_ws/db/fake_data.rb
@@ -10,6 +10,6 @@ reviews = db.from(:reviews)
 	reviews.insert(:image_url => "http://www.#{Faker::Internet.domain_name}/#{rand(99999)}",
 								 :text => Faker::Lorem.paragraph[0..200],
 								 :like => [true, false][rand(2)],
-								 :latitude => 18.51667 + rand/100,
-								 :longitude => 73.91667 + rand/100)
+								 :latitude => 18.51667 + rand/10,
+								 :longitude => 73.91667 + rand/10)
 end

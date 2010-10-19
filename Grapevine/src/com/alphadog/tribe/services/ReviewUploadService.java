1835e5055e126780ca9560d0895083922645d28c
Ravindra R. Jaju <jaju@msync.org>
2010-10-20 00:07:20 +0530

    Continuing with some more simplification - this time around twitter stuff

--- Grapevine/src/com/alphadog/tribe/services/ReviewUploadService.java
+++ Grapevine/src/com/alphadog/tribe/services/ReviewUploadService.java
@@ -28,121 +28,127 @@ import com.alphadog.tribe.models.PendingReview;
 
 public class ReviewUploadService extends WakeEventService {
 
-	private TribeDatabase database;
-	private PendingReviewsTable pendingReviewsTable;
-	private List<PendingReview> pendingReviews;
-	private TweetWithImageUpload imageUploader;
-	private SharedPreferences preferences;
-	
-	@Override
-	public void onCreate() {
-		super.onCreate();
-	}
-	
-	@Override
+    private TribeDatabase database;
+    private PendingReviewsTable pendingReviewsTable;
+    private List<PendingReview> pendingReviews;
+    private TweetWithImageUpload imageUploader;
+    private SharedPreferences preferences;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.i("ReviewsSyncService", "Received start id " + startId + ": " + intent);
         return START_STICKY;
     }
-	
-	@Override
-	public void doServiceTask() {
-		try {
-			database = new TribeDatabase(this);
-			pendingReviewsTable = new PendingReviewsTable(database);
-			
-			preferences = PreferenceManager.getDefaultSharedPreferences(this);
-			Log.i(this.getClass().getName(), "Loading all the pending reviews to be uploaded");
-			loadAllPendingReviews();
-			if(pendingReviews != null && pendingReviews.size() > 0) {
-				
-				String imageUrl = null;
-				
-				List<NameValuePair> payload = new ArrayList<NameValuePair>(10);
-				for(PendingReview eachPendingReview : pendingReviews) {
-					HttpClient httpClient = new DefaultHttpClient();
-					HttpPost postRequest = new HttpPost(getString(R.string.remote_service_url));
-					imageUploader = new TweetWithImageUpload(this);
-					payload.clear();
-					Log.i(this.getClass().getName(), "handling intent for uploading review id: " + eachPendingReview.getId());
-					
-					try {
-						try {
-							imageUrl = uploadImageFor(eachPendingReview);
-						} catch(TwitterCredentialsBlankException tcbe) {
-							imageUrl = "";
-							Log.e(this.getClass().getName(), "Could not upload image to twit4pic because twitter credentials were blank.", tcbe);
-						} 
-						payload.add(new BasicNameValuePair("image_url", imageUrl));
-						payload.add(new BasicNameValuePair("text", eachPendingReview.getHeading()));
-						payload.add(new BasicNameValuePair("like", eachPendingReview.isLike() ? "true" : "false"));
-						payload.add(new BasicNameValuePair("latitude", eachPendingReview.getLatitude()));
-						payload.add(new BasicNameValuePair("longitude", eachPendingReview.getLongitude()));
-						payload.add(new BasicNameValuePair("username", preferences.getString("twitter_username", null)));
-						payload.add(new BasicNameValuePair("created_at", eachPendingReview.getReviewDate()));
-						//Location doesn't make sense right now, so just leaving it for now
-						payload.add(new BasicNameValuePair("location_name", "Unknown"));
-						payload.add(new BasicNameValuePair("token", getString(R.string.request_token)));
-						postRequest.setEntity(new UrlEncodedFormEntity(payload));
-						
-						Log.i(this.getClass().getName(), "posting data: " + payload.toString());
-						HttpResponse response = httpClient.execute(postRequest);					
-						
-						//successfully uploaded to mark pending review as complete
-						if(response.getStatusLine().getStatusCode() == 201) {
-							runPostUploadTaskForReview(eachPendingReview);
-						}
-					} catch(Exception e) {
-						Log.e(this.getClass().getName(), "Exception occured while uploading review with id "+eachPendingReview.getId()+". Error is ", e);
-					}
-					
-				}
-			}
-		}
-		finally {
-			Log.i(this.getClass().getName(), "Stopping service as our job here is done!");
-			//Stop this service as the intended task of the service is done!!
-			stopSelf();
-		}
-	}
 
-	private String uploadImageFor(PendingReview pendingReview) throws TwitterCredentialsBlankException {
-		return imageUploader.uploadImageFor(pendingReview.getImagePath(), pendingReview.getTwitterMessage());		
-	}
+    @Override
+    public void doServiceTask() {
+        try {
+            database = new TribeDatabase(this);
+            pendingReviewsTable = new PendingReviewsTable(database);
 
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		if (database != null)
-			database.close();
-	}
-	
-	private void loadAllPendingReviews() {
-		pendingReviews = pendingReviewsTable.findEligiblePendingReviews();
-	}
-	
-	private void runPostUploadTaskForReview(PendingReview uploadedReview) {
-		deleteImageFileFor(uploadedReview);
-		markUploadedReviewAsComplete(uploadedReview);
-	}
+            preferences = PreferenceManager.getDefaultSharedPreferences(this);
+            Log.i(this.getClass().getName(), "Loading all the pending reviews to be uploaded");
+            loadAllPendingReviews();
+            try {
+                imageUploader = new TweetWithImageUpload(this);
+            }
+            catch (TwitterCredentialsBlankException tcbe) {
+                Log.e(this.getClass().getName(),
+                        "Will not upload images to twit4pic because twitter credentials are blank.", tcbe);
+                imageUploader = null;
+            }
+            if (pendingReviews != null && pendingReviews.size() > 0) {
+                String imageUrl = null;
+                List<NameValuePair> payload = new ArrayList<NameValuePair>(10);
+                for (PendingReview eachPendingReview : pendingReviews) {
+                    HttpClient httpClient = new DefaultHttpClient();
+                    HttpPost postRequest = new HttpPost(getString(R.string.remote_service_url));
 
-	private void markUploadedReviewAsComplete(PendingReview uploadedReview) {
-		Map<String, String> valuesToUpdate = new HashMap<String, String>();
-		valuesToUpdate.put(PendingReviewsTable.PendingReviewCursor.getStatusFieldName(), PendingReviewsTable.COMPLETE_STATUS);
-		pendingReviewsTable.updateFieldsForId(uploadedReview.getId(), valuesToUpdate);
-	}
+                    payload.clear();
+                    Log.i(this.getClass().getName(),
+                            "handling intent for uploading review id: " + eachPendingReview.getId());
+                    try {
+                        imageUrl = uploadImageFor(eachPendingReview);
+                        payload.add(new BasicNameValuePair("image_url", imageUrl == null ? "" : imageUrl));
+                        payload.add(new BasicNameValuePair("text", eachPendingReview.getHeading()));
+                        payload.add(new BasicNameValuePair("like", eachPendingReview.isLike() ? "true" : "false"));
+                        payload.add(new BasicNameValuePair("latitude", eachPendingReview.getLatitude()));
+                        payload.add(new BasicNameValuePair("longitude", eachPendingReview.getLongitude()));
+                        payload.add(new BasicNameValuePair("username", preferences.getString("twitter_username", null)));
+                        payload.add(new BasicNameValuePair("created_at", eachPendingReview.getReviewDate()));
+                        // Location doesn't make sense right now, so just
+                        // leaving it for now
+                        payload.add(new BasicNameValuePair("location_name", "Unknown"));
+                        payload.add(new BasicNameValuePair("token", getString(R.string.request_token)));
+                        postRequest.setEntity(new UrlEncodedFormEntity(payload));
 
-	private void deleteImageFileFor(PendingReview uploadedReview) {
-		Log.i(this.getClass().getName(), "Deleting image for pendingReview");
-		File uploadedPhoto = new File(uploadedReview.getImagePath());
-		//delete the uploaded photo from local disk
-		if(uploadedPhoto != null  && uploadedPhoto.exists()) {
-			uploadedPhoto.delete();
-		}
-	}
-	
-	@Override
-	public IBinder onBind(Intent intent) {
-		return null;
-	}
+                        Log.i(this.getClass().getName(), "posting data: " + payload.toString());
+                        HttpResponse response = httpClient.execute(postRequest);
+
+                        // successfully uploaded to mark pending review as
+                        // complete
+                        if (response.getStatusLine().getStatusCode() == 201) {
+                            runPostUploadTaskForReview(eachPendingReview);
+                        }
+                    }
+                    catch (Exception e) {
+                        Log.e(this.getClass().getName(), "Exception occured while uploading review with id "
+                                + eachPendingReview.getId() + ". Error is ", e);
+                    }
+
+                }
+            }
+        }
+        finally {
+            Log.i(this.getClass().getName(), "Stopping service as our job here is done!");
+            // Stop this service as the intended task of the service is done!!
+            stopSelf();
+        }
+    }
+
+    private String uploadImageFor(PendingReview pendingReview) {
+        if (null == imageUploader) { return null; }
+        return imageUploader.uploadImageFor(pendingReview.getImagePath(), pendingReview.getTwitterMessage());
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (database != null) database.close();
+    }
+
+    private void loadAllPendingReviews() {
+        pendingReviews = pendingReviewsTable.findEligiblePendingReviews();
+    }
+
+    private void runPostUploadTaskForReview(PendingReview uploadedReview) {
+        deleteImageFileFor(uploadedReview);
+        markUploadedReviewAsComplete(uploadedReview);
+    }
+
+    private void markUploadedReviewAsComplete(PendingReview uploadedReview) {
+        Map<String, String> valuesToUpdate = new HashMap<String, String>();
+        valuesToUpdate.put(PendingReviewsTable.PendingReviewCursor.getStatusFieldName(),
+                PendingReviewsTable.COMPLETE_STATUS);
+        pendingReviewsTable.updateFieldsForId(uploadedReview.getId(), valuesToUpdate);
+    }
+
+    private void deleteImageFileFor(PendingReview uploadedReview) {
+        Log.i(this.getClass().getName(), "Deleting image for pendingReview");
+        File uploadedPhoto = new File(uploadedReview.getImagePath());
+        // delete the uploaded photo from local disk
+        if (uploadedPhoto != null && uploadedPhoto.exists()) {
+            uploadedPhoto.delete();
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
